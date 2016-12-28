94e576f513413cc21ad70d8b2299fba37865c5f9
Wu Haotian <whtsky@gmail.com>
2016-12-28 10:02:31 +0800

    Update xuanke.js

--- xuanke.js
+++ xuanke.js
@@ -1,8 +1,21 @@
 function create(profileID, courseID) {
-    const url = `http://eams.uestc.edu.cn/eams/stdElectCourse!batchOperator.action?profileId=${profileID}`
-    const data = `operator0=${courseID}%3Atrue%3A0`
-    setInterval(()=>$.post(url, data, (res) => {
-        const result$ = $(res)
-        console.log(result$.find('div').text().trim())
-    }), 500)
+	const url = `http://eams.uestc.edu.cn/eams/stdElectCourse!batchOperator.action?profileId=${profileID}`
+	function send(data) {
+		$.post(url, data, (res) => {
+			const result$ = $(res)
+			console.log(result$.find('div').text().trim())
+		})
+	}
+	let data
+	if (typeof courseID == "object") {
+		data = courseID.map(x => `operator0=${x}%3Atrue%3A0`)
+		setInterval(() => {
+			for(const d of data) {
+				send(d)
+			}
+		}, 500)
+	} else {
+		data = `operator0=${courseID}%3Atrue%3A0`
+		setInterval(() => send(data), 500)
+	}
 }
