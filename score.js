d0f58d3806ec6f8c65c55ba6fb1c0550574b85f8
Wu Haotian <whtsky@users.noreply.github.com>
2018-07-02 13:27:57 +0800

    Update score.js

--- score.js
+++ score.js
@@ -2,11 +2,14 @@ const scores = {}
 
 const scoreRows = $('.grid table.gridtable tbody tr').toArray()
 scoreRows.forEach(row => {
-    const score = Number(row.children[5].innerText)
-    const category = row.children[4].innerText
-    const className = row.children[3].innerText
-    console.log(`[${category}]: ${className} 学分 ${score}`)
-    scores[category] = (scores[category] || 0) + score
+  if (row.children[8].innerText < 60) {
+    return
+  }
+  const score = Number(row.children[5].innerText)
+  const category = row.children[4].innerText
+  const className = row.children[3].innerText
+  console.log(`[${category}]: ${className} 学分 ${score}`)
+  scores[category] = (scores[category] || 0) + score
 })
 
 console.log(scores)
